9ccf46855c3ea13675253de03b7d985c7b186ccd
BorwankarS <borwankar.saurabh@gmail.com>
2016-12-13 13:23:58 -0800

    Beam active inactive script added

---
+++ Assets/Scripts/SpaceShip.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class SpaceShip : MonoBehaviour
+{
+
+    public GameObject rayLandingPad1, aircraft;
+    int count, activeCount;
+    // Use this for initialization
+    void Start()
+    {
+
+        count = 1;
+        activeCount = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (count % 100 != 0)
+        {
+            count++;
+            rayLandingPad1.SetActive(false);
+        }
+        else if (count % 100 == 0 || activeCount != 0)
+        {
+            activeCount++;
+            if (activeCount < 500)
+            {
+                rayLandingPad1.SetActive(true);
+            }
+            else
+            {
+                Debug.Log(activeCount);
+                activeCount = 0;
+                count++;
+            }
+
+        }
+
+
+    }
+}
---
+++ Assets/rotate.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MeshRenderer mr = GetComponent<MeshRenderer> ();
+		Material m = mr.material;
+		Vector2 offset = m.mainTextureOffset;
+		offset.x += Time.deltaTime / 100f;
+		offset.y += Time.deltaTime / 200f;
+		m.mainTextureOffset = offset;
+	
+	}
+}
